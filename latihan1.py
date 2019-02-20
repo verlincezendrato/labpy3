7d460a63292e695d062e778c52a870fce36b0423
verlincezendrato <verlincezendrato849822@gmail.com>
2019-02-20 13:02:12 +0700

    first commit

---
+++ latihan1.py
@@ -0,0 +1,7 @@
+from random import random
+n=input("masukkan nilai N:")
+a=random()
+while (a<0.1):
+   a= a+1
+for i in range (0,6):
+    print("data ke:",i,"=>",a)
---
+++ latihan2.py
@@ -0,0 +1,8 @@
+max=0
+while True:
+    a=int(input("masukkan bilangan :"))
+    if a==0:
+           break
+    if a>max:
+            max=a
+print("bilangan terbesar ",max)
